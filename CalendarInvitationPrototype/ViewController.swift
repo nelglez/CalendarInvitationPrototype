d13924a4b7c2578ae0d6819f4fcb77ae5c8af02e
Chander bhushan <cbhushan1995@gmail.com>
2019-07-29 18:59:15 +0530

    Initial Commit

---
+++ CalendarInvitationPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarInvitationPrototype
+//
+//  Created by chander bhushan on 29/07/19.
+//  Copyright © 2019 innovationm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
