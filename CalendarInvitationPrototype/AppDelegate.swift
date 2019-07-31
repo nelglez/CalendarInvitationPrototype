f6409b307f393093abb5a8667646a66891ff3f1f
Chander bhushan <cbhushan1995@gmail.com>
2019-07-31 10:37:30 +0530

    Entitlement Problem Resolved!

--- CalendarInvitationPrototype/AppDelegate.swift
+++ CalendarInvitationPrototype/AppDelegate.swift
@@ -20,19 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         CommonData.shared.loadData()
         
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
+        
         // add actions for notifications
         // 1
         
@@ -50,6 +38,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 3
         UNUserNotificationCenter.current().setNotificationCategories([eventsCategory])
 
+        
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
         Messaging.messaging().delegate = self
         application.registerForRemoteNotifications()
         
